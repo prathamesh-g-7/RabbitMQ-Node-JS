152af88c04f623b50ba6582a7fcb1cfecee383b2
prathamesh-g-7 <prathameshgaikwad13999@gmail.com>
2021-10-05 15:30:44 +0530

    completed workshop 15

---
+++ backend/Sender.js
@@ -0,0 +1,28 @@
+// advanced messege queue protocol
+const amqp = require("amqplib");
+
+const msg = {
+  number: process.argv[2],
+};
+
+connect();
+
+async function connect() {
+  try {
+    const amqpServer = "amqp://localhost:5672";
+    const connection = await amqp.connect(amqpServer);
+    const channel = await connection.createChannel();
+
+    //
+    await channel.assertQueue("jobs");
+    await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+
+    //
+    console.log(`Job sent successfully => ${msg.number}`);
+
+    await channel.close();
+    await connection.close();
+  } catch (err) {
+    console.error(err);
+  }
+}
---
+++ backend/receiverOne.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib");
+
+connect();
+async function connect() {
+  try {
+    const amqpServer = "amqp://localhost:5672";
+    const connection = await amqp.connect(amqpServer);
+    const channel = await connection.createChannel();
+
+    //
+    await channel.assertQueue("jobs");
+
+    channel.consume("jobs", (message) => {
+      const input = JSON.parse(message.content.toString());
+      console.log(`Recieved job with input ${input.number}`);
+    });
+
+    console.log("Waiting for messages...");
+  } catch (err) {
+    console.error(err);
+  }
+}
